af2231407619668a5a22275faf53907013960f2b
ANDYS_LAPTOP\andyb <andybryanlam@gmail.com>
2023-01-12 08:11:13 +0100

    Initial commit

---
+++ src/main/java/TLOZMain.java
@@ -0,0 +1,15 @@
+public class TLOZMain {
+    public static void main(String[] args) {
+        System.out.println("     /\\");
+        System.out.println("    /  \\");
+        System.out.println("   /____\\");
+        System.out.println("  /\\    /\\");
+        System.out.println(" /  \\  /  \\");
+        System.out.println("/____\\/____\\");
+        System.out.println("Welcome to");
+        System.out.println("\"The Legends of Zelda: Zork of the Wild\"");
+        System.out.println("");
+        System.out.println("Start");
+        System.out.println("Quit");
+    }
+}
