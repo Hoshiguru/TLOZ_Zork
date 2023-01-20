fa9adaf0cb642ce8301f39282619aaa24c841ff1
kaiseryao <dobie_lilesville@relay.ywk.ch>
2023-01-20 23:12:35 +0100

    more comments

--- src/main/java/game/cmds/ScoreCommand.java
+++ src/main/java/game/cmds/ScoreCommand.java
@@ -7,6 +7,7 @@ public class ScoreCommand {
     private Player player;
     public void score(Player player){
         System.out.println("There is not much to see here yet.");
+        // TODO: Show Hearts as Icon instead of text
         System.out.println("Health: " + player.getHearts());
     }
 }
