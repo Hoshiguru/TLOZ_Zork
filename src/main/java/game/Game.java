dbf945f4d09c663bd5f12a38bf8389f1d7b925d8
Yao Kaiser <yao@ywk.ch>
2023-01-21 17:36:47 +0100

    refactor: new game class structure

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -13,12 +13,44 @@ public class Game {
 
     //private Place places; // oder auch Räume
     public void startGame() {
-        // Hier wird alles initialisiert
+        player = new Player(3, null, 20.0, null);
         commandHandler = new CommandHandler();
+        Scanner scanner = new Scanner(System.in);
+        String command;
+        System.out.println("     /\\");
+        System.out.println("    /  \\");
+        System.out.println("   /____\\");
+        System.out.println("  /\\    /\\");
+        System.out.println(" /  \\  /  \\");
+        System.out.println("/____\\/____\\");
+        System.out.println("Welcome to");
+        System.out.println("\"The Legends of Zelda: Zork of the Wild\"");
+        System.out.println("");
+        System.out.println("Type start to start the game");
+        System.out.print("> ");
 
+        while (!(command = scanner.nextLine()).equalsIgnoreCase("start")) {
+            System.out.println("Invalid command. Please type 'start' to begin the game.");
+            System.out.print("> ");
+        }
+        initializeGame();
+        System.out.println("Link, are you awake? You're currently in the Shrine of Life. Walk in direction to north, to exit.");
+        // Hier startet das Spiel
+        while (true) {
+            System.out.print("> ");
+            command = scanner.nextLine();
+            try {
+                commandHandler.handleCommand(command, player);
+            } catch (InvalidCommandException e) {
+                System.out.println(e.getMessage());
+            } catch (InvalidDirectionException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 
-        // TODO: Schreine hinzufügen
-        // Initialisierung Items
+    private void initializeGame() {
+        // Hier wird alles initialisiert
         Item bow_and_arrow = new Item("Bow and Arrow", "A ranged weapon used to defeat enemies and hit distant targets.", 1.2);
         Item iron_sword = new Item("Iron Sword", "A melee weapon used to defeat enemies and hit close targets.", 1.8);
         Item shield = new Item("Shield", "A defensive item used to protect the player from enemy attacks.", 6.5);
@@ -26,7 +58,7 @@ public class Game {
 
         // Initialisierung Räume
         castle_ruin = new Location("Castle Ruin", "\uD83C\uDFDB", "A mysterious, crumbling castle awaits exploration, filled with dangerous enemies and valuable treasures.", "castle_ruin", null);
-        woodland = new Location("Woodland", "\uD83C\uDF33", "A dense forest filled with dangerous enemies and valuable treasures. game.Location of the master sword.", "woodland", iron_sword);
+        woodland = new Location("Woodland", "\uD83C\uDF33", "A dense forest filled with dangerous enemies and valuable treasures. Location of the master sword.", "woodland", iron_sword);
         castle = new Location("Castle", "\uD83C\uDFF0", "A grand and imposing castle stands at the center of the kingdom, guarded by powerful enemies and holding secrets of ancient power.", "castle", null);
         cave = new Location("Cave", "\uD83E\uDEA8", "A dark and treacherous cave system winds deep into the earth, filled with dangerous creatures and hidden treasures.", "cave", shield);
         desert = new Location("Desert", "\uD83C\uDFDC️", "A vast and scorching desert stretches as far as the eye can see, with hidden oases, ancient ruins, and deadly sandstorms.", "desert", bow_and_arrow);
@@ -47,36 +79,11 @@ public class Game {
         desert.setDirections(null, null, gateDesertUnderwater_temple, gateCaveDesert);
         underwater_temple.setDirections(gateDesertUnderwater_temple, null, null, null);
 
-        player = new Player(3, null, 20.0, castle_ruin);
-
-        // Hier startet das Spiel
-        Scanner scanner = new Scanner(System.in);
-        String command;
-
-        System.out.println("     /\\");
-        System.out.println("    /  \\");
-        System.out.println("   /____\\");
-        System.out.println("  /\\    /\\");
-        System.out.println(" /  \\  /  \\");
-        System.out.println("/____\\/____\\");
-        System.out.println("Welcome to");
-        System.out.println("\"The Legends of Zelda: Zork of the Wild\"");
-        System.out.println("");
-        System.out.println("Type start to start the game");
-
-        boolean gameWon = false;
-        while (gameWon != true) {
-            System.out.print("> ");
-            command = scanner.nextLine();
-            try {
-                commandHandler.handleCommand(command, player);
-            } catch (InvalidCommandException e) {
-                System.out.println(e.getMessage());
-            } catch (InvalidDirectionException e) {
-                System.out.println("Please enter a valid direction • (n)orth, (e)ast, (s)outh, (w)est)");
-            }
-        }
+        // Startposition festlegen
+        player.setCurrentLocation(castle_ruin);
     }
+
+
 
 
 }
