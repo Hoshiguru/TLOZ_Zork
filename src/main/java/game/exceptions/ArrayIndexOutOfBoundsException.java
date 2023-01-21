eba82305d87da93e2cd467c5ccde084e9e3f2daf
Yao Kaiser <yao@ywk.ch>
2023-01-21 18:50:20 +0100

    remove index exception

--- src/main/java/game/exceptions/ArrayIndexOutOfBoundsException.java
+++
@@ -1,7 +0,0 @@
-package game.exceptions;
-
-public class ArrayIndexOutOfBoundsException extends Exception {
-    public ArrayIndexOutOfBoundsException(String message) {
-        super(message);
-    }
-}
