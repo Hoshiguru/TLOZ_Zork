97abd0da51f0e2e3aa357696751e3aec6e027a3d
Abbex Nox <dobie_lilesville@relay.ywk.ch>
2023-01-24 19:20:44 +0100

    refactor: split into multiple methods

--- src/main/java/ch/bbw/tloz_zork/cmds/MoveCommand.java
+++ src/main/java/ch/bbw/tloz_zork/cmds/MoveCommand.java
@@ -12,8 +12,29 @@ public class MoveCommand {
      * @param direction
      */
     public void move(Player player, String direction){
-        // Get the current game.Location
         Location currentLocation = player.getCurrentLocation();
+        // Get the game.Gate of the current game.Location
+        Gate gate = currentLocation.getDirections().get(getFullDirection(direction));
+        // Check if the game.Gate is null
+        if(gate == null){
+            System.out.println("You can't go there!");
+        }else if (!gate.isBlocked()){
+            player.increaseMoves();
+            Location nextLocation = getNextLocation(currentLocation, gate);
+            player.setPreviousLocation(currentLocation);
+            player.setCurrentLocation(nextLocation);
+            System.out.println(nextLocation.getIcon() + "You are now in " + nextLocation.getName());
+            System.out.println(nextLocation.getQuote());
+            checkEnemyStatus(nextLocation);
+            if (nextLocation instanceof Dungeon) {
+                Dungeon dungeon = (Dungeon) nextLocation;
+                dungeon.startChallenge(player);
+            }
+        } else {
+            System.out.println("You can\'t go there.");
+        }
+    }
+    private String getFullDirection(String direction) {
         switch (direction) {
             case "n" -> direction = "north";
             case "s" -> direction = "south";
@@ -22,51 +43,26 @@ public class MoveCommand {
             default -> {
             }
         }
-        // Get the game.Gate of the current game.Location
-        Gate gate = currentLocation.getDirections().get(direction);
-        // Check if the game.Gate is null
-        if(gate == null){
-            System.out.println("You can't go there!");
-        }else if (!gate.isBlocked()){
-            player.increaseMoves();
-            Location location1 = gate.getLocation1();
-            Location location2 = gate.getLocation2();
-            // TODO: Versuchen, das in einer Methode zu machen
-            if (location1.getName().equals(currentLocation.getName())) {
-                player.setPreviousLocation(location1);
-                player.setCurrentLocation(location2);
-                System.out.println(location2.getIcon() + "You are now in " + location2.getName());
-                System.out.println(location2.getQuote());
-                // TODO: Eventuell in Location auslagern e.g. location.checkEnemyStatus
-                if (location2.getEnemy() != null) {
-                    System.out.println("⚠️There is also a " + location2.getEnemy().getName() + " in this area. This enemy holds a " + location2.getEnemy().getItem().getName() + ".\nYou can 'fight' him anytime while you are in this Location");
-                }
-                if (location2 instanceof Dungeon) {
-                    Dungeon dungeon = (Dungeon) location2;
-                    dungeon.startChallenge(player);
-                }
-            }
-            else if (location2.getName().equals(currentLocation.getName())){
-                player.setPreviousLocation(location2);
-                player.setCurrentLocation(location1);
-                System.out.println(location1.getIcon() + "You are now in " + location1.getName());
-                System.out.println(location1.getQuote());
-                if (location1.getEnemy() != null) {
-                    System.out.println("There is also a " + location1.getEnemy().getName() + " in this area. This enemy holds a " + location1.getEnemy().getItem().getName() + ".\nYou can 'fight' him anytime while you are in this Location");
-                }
-                if (location1 instanceof Dungeon) {
-                    Dungeon dungeon = (Dungeon) location1;
-                    dungeon.startChallenge(player);
-                }
-            }
-            else {
-                System.out.println("Hmm. For whatever reason, you can\'t pass here");
-            }
-        }
-        else {
-            System.out.println("You can\'t go there.");
+        return direction;
+    }
+    private Location getNextLocation(Location currentLocation, Gate gate) {
+        Location location1 = gate.getLocation1();
+        Location location2 = gate.getLocation2();
+        if (location1.getName().equals(currentLocation.getName())) {
+            return location2;
+        } else if (location2.getName().equals(currentLocation.getName())){
+            return location1;
+        } else {
+            System.out.println("Hmm. For whatever reason, you can\'t pass here");
+            return currentLocation;
         }
     }
+    private void checkEnemyStatus(Location location) {
+        if (location.getEnemy() != null && !location.getEnemy().getIsDead()){
+            System.out.println("⚠️There is also a " + location.getEnemy().getName() + " in this area. This enemy holds a " + location.getEnemy().getItem().getName() + ".\nYou can 'fight' him anytime while you are in this Location");
+        }
+    }
+
     /**
      * Move the player to the previous Location
      * @param player
