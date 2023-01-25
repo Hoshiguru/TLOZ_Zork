4afb62ccc39d24562eff07ab9d142b546f27a0fb
Abbex Nox <dobie_lilesville@relay.ywk.ch>
2023-01-25 01:23:30 +0100

    hotfix: handled exceptions, which interrupt game

--- src/main/java/ch/bbw/tloz_zork/cmds/CommandHandler.java
+++ src/main/java/ch/bbw/tloz_zork/cmds/CommandHandler.java
@@ -34,11 +34,15 @@ public class CommandHandler {
      */
     public void handleCommand(String command, Player player) throws InvalidCommandException, InvalidDirectionException, IOException {
         if (command.startsWith("move") || command.startsWith("walk")) {
-            String input = command.substring(4);
-            String[] parts = input.split(" ");
-            if (parts.length > 1) {
-                String direction = parts[1];
-                moveCommand.move(player, direction);
+            if (command.length() > 4) {
+                String input = command.substring(4);
+                String[] parts = input.split(" ");
+                if (parts.length > 1) {
+                    String direction = parts[1];
+                    moveCommand.move(player, direction);
+                } else {
+                    System.out.println("Please tell, where you want to go. Type 'move <direction>' to walk.");
+                }
             } else {
                 System.out.println("Please tell, where you want to go. Type 'move <direction>' to walk.");
             }
@@ -53,29 +57,39 @@ public class CommandHandler {
         } else if (command.equals("grab") || command.equals("g")) {
             itemCommands.grab(player);
         } else if (command.startsWith("drop")) {
-            String input = command.substring(4);
-            String[] parts = input.split(" ");
-            if (parts.length > 1) {
-                String item = parts[1];
-                itemCommands.drop(player, item);
+            if (command.length() > 4) {
+                String input = command.substring(4);
+                String[] parts = input.split(" ");
+                if (parts.length > 1) {
+                    String item = parts[1];
+                    itemCommands.drop(player, item);
+                } else {
+                    System.out.println("Please provide an item to drop. Type 'drop <item>' to drop an item.");
+                }
             } else {
                 System.out.println("Please provide an item to drop. Type 'drop <item>' to drop an item.");
             }
         } else if (command.startsWith("eat") || command.startsWith("e")) {
-            String input = command.substring(3);
-            String[] parts = input.split(" ");
-            if (parts.length > 1) {
-                String item = parts[1];
-                itemCommands.eat(player, item);
+            if (command.length() > 3) {
+                String input = command.substring(3);
+                String[] parts = input.split(" ");
+                if (parts.length > 1) {
+                    String item = parts[1];
+                    itemCommands.eat(player, item);
+                } else {
+                    System.out.println("Please provide an item to eat. Type 'eat <item>' to eat an item.");
+                }
             } else {
                 System.out.println("Please provide an item to eat. Type 'eat <item>' to eat an item.");
             }
         } else if (command.startsWith("use") || command.startsWith("u")) {
-            String input = command.substring(3);
-            String[] parts = input.split(" ");
-            if (parts.length > 1) {
-                String item = parts[1];
-                itemCommands.use(player, item);
+            if (command.length() > 3) {
+                String input = command.substring(3);
+                String[] parts = input.split(" ");
+                if (parts.length > 1) {
+                    String item = parts[1];
+                    itemCommands.use(player, item);
+                }
             } else {
                 System.out.println("Please provide an item to eat. Type 'use <item>' to equip an weapon.");
             }
@@ -83,7 +97,7 @@ public class CommandHandler {
             itemCommands.inventory(player);
         } else if (command.equals("fight") || command.equals("f")) {
             // Can only be entered a fight if there is an enemy at the Location
-            if (player.getCurrentLocation().getEnemy().getHealth() > 0) {
+            if (player.getCurrentLocation().getEnemy() != null) {
                 combatCommand.combat(player, player.getCurrentLocation().getEnemy(), false);
             } else {
                 // Else it prints out this
